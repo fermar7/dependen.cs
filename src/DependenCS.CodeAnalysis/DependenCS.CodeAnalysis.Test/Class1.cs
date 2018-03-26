9a986832dfbae2cfbe441f3082612949df539d20
fermar7 <fermar0715@gmail.com>
2018-03-26 15:55:56 +0200

    Added folders and inital code analysis project structure

---
+++ src/DependenCS.CodeAnalysis/DependenCS.CodeAnalysis.Test/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DependenCS.CodeAnalysis.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
