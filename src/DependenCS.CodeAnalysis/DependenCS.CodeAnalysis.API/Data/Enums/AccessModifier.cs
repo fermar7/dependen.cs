1ade1fe73fc579b83db760ae50c759edc2120fc0
fermar7 <fermar0715@gmail.com>
2018-03-28 16:33:51 +0200

    AccessModifier enum

---
+++ src/DependenCS.CodeAnalysis/DependenCS.CodeAnalysis.API/Data/Enums/AccessModifier.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DependenCS.CodeAnalysis.API.Data.Enums
+{
+    public enum AccessModifier
+    {
+        Public,
+        Protected,
+        Internal,
+        Private,
+        ProtecedInternal,
+        PrivateProtected
+    }
+}
